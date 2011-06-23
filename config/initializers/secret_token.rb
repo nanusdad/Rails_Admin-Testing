aedf9077cb05c990f6bdf8d4ae6e7f888347840e
Arun Zechariah <arun@zechariah.net>
2011-06-22 23:17:50 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testra::Application.config.secret_token = '6e7d262947567d2e74a7bfe389caf8b7c8f721f0d9d793beaa81b7190b4018a7caa392d942ca8e5ca7b173ee660a8bcd222dd76d5f67cd8401e590b26aec9f11'
